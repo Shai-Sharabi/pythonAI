87e3cfce415c75634aecd84ff966149782329f40
Shai-Sharabi <92423685+Shai-Sharabi@users.noreply.github.com>
2023-08-11 16:57:21 +0300

    Update a.py

--- .github/workflows/a.py
+++ .github/workflows/a.py
@@ -1,16 +1,26 @@
 print("Hello Simon!")
-a1=[]
-a2=[]
-a3=[]
-a1
-with open(r'c:\Users\IMOE001\downloads\assign1_data.txt_\assign1_data.txt') as f:
-	next(f) # skip 1 line
-	next(f) # skip 1 line
-	for line in f:
-		data=line.split()
-		a1.append(float(data[0]))
-		a2.append(float(data[1]))
-		a3.append(float(data[2]))
-        
-"""for i in a1:
-    print(i, end = ' ')"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create some example data
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 5, 7, 7, 10])
+
+# Plot the array of dots
+plt.scatter(x, y, color='red', label='Dots')
+
+# Fit a linear regression line
+slope, intercept = np.polyfit(x, y, 1)
+linear_fit = slope * x + intercept
+
+# Plot the linear graph
+plt.plot(x, linear_fit, color='blue', label='Linear Fit')
+
+# Add labels and legend
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Array of Dots and Linear Graph')
+plt.legend()
+
+# Display the plot
+plt.show()
